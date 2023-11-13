dc4e52ad0984389eee59ce378b034e818d41fe57
osawa-koki <koki@osawa.dev>
2023-11-13 20:28:11 +0900

    D&Dソートの実装。

---
+++ pages/vanilla.tsx
@@ -0,0 +1,84 @@
+import fs from 'fs'
+import React, { useEffect, useState } from 'react'
+import { Button, ListGroup } from 'react-bootstrap'
+
+interface Props {
+  fruits: string[]
+}
+
+export default function vanillaPage (props: Props): React.JSX.Element {
+  const { fruits: presetFruits } = props
+
+  const [fruits, setFruits] = useState<string[] | null>(null)
+
+  const [dragging, setDragging] = useState<string | null>(null)
+
+  const handleDragStart = (e: React.DragEvent<HTMLLIElement>): void => {
+    setDragging(e.currentTarget.id)
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLLIElement>): void => {
+    e.preventDefault()
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLLIElement>): void => {
+    if (dragging === null || fruits == null) return
+    e.preventDefault()
+    const fruitIndex = fruits.indexOf(dragging)
+    const dropIndex = fruits.indexOf(e.currentTarget.id)
+    const newFruits = [...fruits]
+    newFruits.splice(fruitIndex, 1)
+    newFruits.splice(dropIndex, 0, dragging)
+    setFruits(newFruits)
+  }
+
+  useEffect(() => {
+    const savedFruitsString = localStorage.getItem('fruits')
+    if (savedFruitsString != null) {
+      setFruits(JSON.parse(savedFruitsString))
+    } else {
+      setFruits(presetFruits)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (fruits != null) {
+      localStorage.setItem('fruits', JSON.stringify(fruits))
+    }
+  }, [fruits])
+
+  if (fruits === null) return (<div>Loading...</div>)
+
+  return (
+    <>
+      <h1>Drag and Drop Sort</h1>
+      <ListGroup>
+        {fruits.map((fruit, index) => (
+          <ListGroup.Item
+            key={index}
+            id={fruit}
+            draggable="true"
+            onDragStart={handleDragStart}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+            role="button"
+          >
+            {fruit}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <hr />
+      <Button onClick={() => { setFruits(presetFruits) }}>Reset</Button>
+    </>
+  )
+}
+
+export function getStaticProps (): { props: Props } {
+  const fruitsString = fs.readFileSync('./data/fruits.json', 'utf-8')
+  const fruits = JSON.parse(fruitsString)
+  return {
+    props: {
+      fruits
+    }
+  }
+}
